5c1e494a3c92f43bd651728edeefa16ff07ee16e
amberlie03 <149625016+amberlie03@users.noreply.github.com>
2026-02-04 15:59:26 +0000

    Update schema.sql with mutations table and QC

--- schema.sql
+++ schema.sql
@@ -18,6 +18,10 @@ CREATE TABLE IF NOT EXISTS experiments (
     -- Wild-type protein amino acid sequence inserted by user.
     wt_dna_sequence TEXT NOT NULL,
     -- Wild-type plasmid DNA sequence, inserted by user.
+    uniprot_features JSONB,
+    plasmid_validated BOOLEAN DEFAULT FALSE,
+    validation_note TEXT,
+    metadata JSONB,
 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
@@ -31,19 +35,26 @@ CREATE TABLE IF NOT EXISTS variants (
     variant_id SERIAL PRIMARY KEY,
     experiment_id INTEGER NOT NULL,
     plasmid_variant_index INTEGER,
-    parent_plasmid_variant INTEGER,
+    parent_variant_id INTEGER,
     -- Not sure if this is needed as this tracks the previous generation variant
-    generation INTEGER,
+    generation INTEGER NOT NULL,
     assembled_dna_sequence TEXT NOT NULL,
     -- Represents the plasmid variants
     protein_sequence TEXT NOT NULL,
     activity_score REAL,
     -- Calculated after parsing/QC
     mutation_total INTEGER,
+    qc_passed BOOLEAN DEFAULT TRUE,
+    qc_reason TEXT,
+    -- Can be removed if not needed
+    metadata JSONB,
 
     CONSTRAINT fk_variant_experiment FOREIGN KEY (experiment_id)
     REFERENCES experiments(experiment_id) ON DELETE CASCADE
     -- Ensures FK is taken from the experiments table
+
+    CONSTRAINT fk_parent_variant FOREIGN KEY (parent_variant_id)
+    REFERENCES variants(variant_id)
 );
  
 CREATE TABLE IF NOT EXISTS measurements (
@@ -54,9 +65,22 @@ CREATE TABLE IF NOT EXISTS measurements (
     -- e.g. DNA_Quantification_fg
     protein_yield FLOAT,
     -- e.g. Protein_Quantification_fg
-    is_control BOOLEAN,
+    is_control BOOLEAN DEFAULT FALSE,
     -- Will be TRUE if it is a control WT measurement
 
     CONSTRAINT fk_measurement_variant FOREIGN KEY (variant_id)
     REFERENCES variants(variant_id) ON DELETE CASCADE
  );
+
+CREATE TABLE IF NOT EXISTS mutations (
+    mutation_id SERIAL PRIMARY KEY,
+    variant_id INTEGER NOT NULL,
+    position INTEGER NOT NULL,
+    wt_residue CHAR(1),
+    mutant_residue CHAR(1),
+    -- To identify the type of mutation
+    mutation_type VARCHAR(20),
+
+    CONSTRAINT fk_variant_mutation FOREIGN KEY (variant_id)
+    REFERENCES variants(variant_id) ON DELETE CASCADE
+);
